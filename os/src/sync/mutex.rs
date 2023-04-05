f12710c41f8b08aebdd71d8340fced101f8dc6a1
sslime336 <sslime@yeah.net>
2023-04-05 17:12:13 +0800

    add(mutex): a hand-made spin mutex without testing

--- os/src/sync/mutex.rs
+++ os/src/sync/mutex.rs
@@ -1,88 +1,76 @@
-use super::UPSafeCell;
-use crate::task::TaskControlBlock;
-use crate::task::{add_task, current_task};
-use crate::task::{block_current_and_run_next, suspend_current_and_run_next};
+use core::{
+    borrow::{Borrow, BorrowMut},
+    cell::UnsafeCell,
+    marker::PhantomData,
+    ops::Deref,
+    ops::DerefMut,
+    sync::atomic::{AtomicBool, Ordering},
+};
+
 use alloc::{collections::VecDeque, sync::Arc};
 
-pub trait Mutex: Sync + Send {
-    fn lock(&self);
-    fn unlock(&self);
+pub type Mutex<T> = SpinMutex<T>;
+
+/// 自旋锁（当前 os 中锁的默认实现）
+///
+/// 未获得锁的进程将进入自旋状态，直到获得锁
+#[repr(C)]
+pub struct SpinMutex<T: ?Sized> {
+    /// 是否上锁
+    locked: AtomicBool,
+
+    data: UnsafeCell<T>,
 }
 
-pub struct MutexSpin {
-    locked: UPSafeCell<bool>,
-}
+unsafe impl<T: ?Sized + Send> Sync for SpinMutex<T> {}
+unsafe impl<T: ?Sized + Send> Send for SpinMutex<T> {}
 
-impl MutexSpin {
-    pub fn new() -> Self {
+impl<T> SpinMutex<T> {
+    pub fn new(val: T) -> Self {
         Self {
-            locked: unsafe { UPSafeCell::new(false) },
-        }
-    }
-}
-
-impl Mutex for MutexSpin {
-    fn lock(&self) {
-        loop {
-            let mut locked = self.locked.exclusive_access();
-            if *locked {
-                drop(locked);
-                suspend_current_and_run_next();
-                continue;
-            } else {
-                *locked = true;
-                return;
-            }
+            locked: AtomicBool::new(false),
+            data: UnsafeCell::new(val),
         }
     }
 
-    fn unlock(&self) {
-        let mut locked = self.locked.exclusive_access();
-        *locked = false;
+    /// 获得锁之前会一直自旋
+    pub fn lock(&self) -> MutexGuard<T> {
+        while self.locked.load(Ordering::SeqCst) {}
+
+        MutexGuard { lock: self }
     }
 }
 
-pub struct MutexBlocking {
-    inner: UPSafeCell<MutexBlockingInner>,
-}
-
-pub struct MutexBlockingInner {
-    locked: bool,
-    wait_queue: VecDeque<Arc<TaskControlBlock>>,
-}
-
-impl MutexBlocking {
-    pub fn new() -> Self {
-        Self {
-            inner: unsafe {
-                UPSafeCell::new(MutexBlockingInner {
-                    locked: false,
-                    wait_queue: VecDeque::new(),
-                })
-            },
+impl<T: ?Sized> SpinMutex<T> {
+    pub fn unlock(&self) {
+        // 未上锁
+        if !self.locked.load(Ordering::SeqCst) {
+            panic!("try unlock on an unlocked mutex!");
         }
+        self.locked.store(false, Ordering::SeqCst);
     }
 }
 
-impl Mutex for MutexBlocking {
-    fn lock(&self) {
-        let mut mutex_inner = self.inner.exclusive_access();
-        if mutex_inner.locked {
-            mutex_inner.wait_queue.push_back(current_task().unwrap());
-            drop(mutex_inner);
-            block_current_and_run_next();
-        } else {
-            mutex_inner.locked = true;
-        }
-    }
+pub struct MutexGuard<'a, T: ?Sized + 'a> {
+    lock: &'a Mutex<T>,
+}
 
-    fn unlock(&self) {
-        let mut mutex_inner = self.inner.exclusive_access();
-        assert!(mutex_inner.locked);
-        if let Some(waking_task) = mutex_inner.wait_queue.pop_front() {
-            add_task(waking_task);
-        } else {
-            mutex_inner.locked = false;
-        }
+impl<T: ?Sized> Deref for MutexGuard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.lock.data.get() }
+    }
+}
+
+impl<T: ?Sized> DerefMut for MutexGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.lock.data.get() }
+    }
+}
+
+impl<T: ?Sized> Drop for MutexGuard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.unlock();
     }
 }
