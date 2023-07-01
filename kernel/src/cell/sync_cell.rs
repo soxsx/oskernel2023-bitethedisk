c5272918a23aa133ab210c530174e02757ab4b2a
sslime336 <sslime@yeah.net>
2023-07-01 21:20:45 +0800

    Move: SyncRefCell to sync_cell.rs

---
+++ kernel/src/cell/sync_cell.rs
@@ -0,0 +1,25 @@
+use core::cell::{Ref, RefCell, RefMut};
+
+pub struct SyncRefCell<T> {
+    inner: RefCell<T>,
+}
+
+impl<T> SyncRefCell<T> {
+    pub fn borrow(&self) -> Ref<'_, T> {
+        self.inner.borrow()
+    }
+
+    pub fn borrow_mut(&self) -> RefMut<'_, T> {
+        self.inner.borrow_mut()
+    }
+}
+
+unsafe impl<T> Sync for SyncRefCell<T> {}
+
+impl<T> SyncRefCell<T> {
+    pub fn new(value: T) -> Self {
+        Self {
+            inner: RefCell::new(value),
+        }
+    }
+}
