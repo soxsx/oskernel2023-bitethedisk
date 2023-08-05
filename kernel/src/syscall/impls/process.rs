f855bf424e8051da973b7e535300d5226f9ca9bf
ll <>
2023-08-06 02:40:55 +0800

    Fix: cross page

--- kernel/src/syscall/impls/process.rs
+++ kernel/src/syscall/impls/process.rs
@@ -757,10 +757,11 @@ pub fn sys_sigaction(signum: isize, act: *const SigAction, oldact: *mut SigActio
     if act as usize != 0 {
         let mut sigaction = task.sigactions.write();
         if oldact as usize != 0 {
-            *translated_mut(token, oldact) = sigaction[signum as usize];
+            copyout(token, oldact, &sigaction[signum as usize]);
         }
         //在 pcb 中注册给定的 signaction
-        let mut sa = translated_ref(token, act).clone();
+        let mut sa = SigAction::new();
+        copyin(token, &mut sa, act);
         // kill 和 stop 信号不能被屏蔽
         sa.sa_mask.sub(Signal::SIGKILL as u32); // sub 函数保证即使不存在 SIGKILL 也无影响
         sa.sa_mask.sub(Signal::SIGSTOP as u32);
